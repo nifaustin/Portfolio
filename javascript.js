6f5228fbd411fc2f958bfcf52c789afaa46fc2c5
nifaustin <niyomukizafaustin@gmail.com>
2023-09-08 11:00:48 +0200

    my portifolio

---
+++ javascript.js
@@ -0,0 +1,50 @@
+const openMenu =  document.getElementById('openMenu')
+const closeMenu =  document.getElementById('closeMenu')
+const Container =  document.getElementById('menuCard')
+const link =  document.getElementById('link')
+const link1 =  document.getElementById('link1')
+const link2 =  document.getElementById('link2')
+const link3 =  document.getElementById('link3')
+const link4 =  document.getElementById('link4')
+const link5 =  document.getElementById('link5')
+const link6 =  document.getElementById('link6')
+
+
+openMenu.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "0px"
+})
+
+closeMenu.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
+
+link.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
+link1.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
+link2.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
+link3.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
+link4.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
+link5.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
+link6.addEventListener('click', () => {
+    Container.style.transition = "0.5s"
+    Container.style.marginLeft = "-200vw"
+})
